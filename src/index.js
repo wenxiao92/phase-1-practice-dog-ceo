90e530d220eac125acacb3f1a18bcc6a7d824db0
Wen Xiao <wennstyle@gmail.com>
2021-10-21 23:02:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+let imgContainer = document.querySelector('#dog-image-container')
+let array
+document.addEventListener("DOMContentLoaded", grabImages)
+
+function grabImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data){
+        array = data
+        console.log(array)
+        for(let images of array) {
+            let img = document.createElement("img")
+            img.setAttribute("src", `${images}`)
+        }
+    })
+}
+
+// function displayImg(theImage) {
+//     let images = document.createElement("img")
+//     images.setAttribute("src", `${theImage}`)
+// }
